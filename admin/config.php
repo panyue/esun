caaa763a72ddbb58b0b73acb991ce6a81bc0b1f6
panyue <iamy.id@gmail.com>
2012-08-23 14:49:12 +0800

    test

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:1986/admin/');
+define('HTTP_CATALOG', 'http://localhost:1986/');
+define('HTTP_IMAGE', 'http://localhost:1986/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:1986/admin/');
+define('HTTPS_CATALOG', 'http://localhost:1986/');
+define('HTTPS_IMAGE', 'http://localhost:1986/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Yue/Documents/code/web/admin/');
+define('DIR_SYSTEM', '/Users/Yue/Documents/code/web/system/');
+define('DIR_DATABASE', '/Users/Yue/Documents/code/web/system/database/');
+define('DIR_LANGUAGE', '/Users/Yue/Documents/code/web/admin/language/');
+define('DIR_TEMPLATE', '/Users/Yue/Documents/code/web/admin/view/template/');
+define('DIR_CONFIG', '/Users/Yue/Documents/code/web/system/config/');
+define('DIR_IMAGE', '/Users/Yue/Documents/code/web/image/');
+define('DIR_CACHE', '/Users/Yue/Documents/code/web/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Yue/Documents/code/web/download/');
+define('DIR_LOGS', '/Users/Yue/Documents/code/web/system/logs/');
+define('DIR_CATALOG', '/Users/Yue/Documents/code/web/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'sunandsu_pnc1');
+define('DB_PREFIX', 'oc_');
+?>
