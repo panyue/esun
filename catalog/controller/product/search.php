cc38ee0f9856bb96efbdb04c84da03b1467bdaf7
panyue <iamy.id@gmail.com>
2012-10-03 05:08:37 -0700

    modify search page with the style complete

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -249,6 +249,7 @@ class ControllerProductSearch extends Controller {
 				$this->data['products'][] = array(
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
+					'model'       => $result['model'],
 					'name'        => $result['name'],
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
 					'price'       => $price,
