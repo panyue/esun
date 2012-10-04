0b34f0d9429768a49ead509208cb8356ee019498
panyue <iamy.id@gmail.com>
2012-10-03 21:48:21 -0700

    修改gtalk为online contact

--- catalog/controller/module/google_talk.php
+++ catalog/controller/module/google_talk.php
@@ -1,9 +1,14 @@
 <?php  
 class ControllerModuleGoogleTalk extends Controller {
 	protected function index() {
-		$this->language->load('module/google_talk');
+		$this->language->load('information/contact');
 
       	$this->data['heading_title'] = $this->language->get('heading_title');
+		
+		$this->data['msn'] = $this->config->get('config_msn');
+		$this->data['qq'] = $this->config->get('config_qq');
+		$this->data['skype'] = $this->config->get('config_skype');
+		$this->data['gtalk'] = $this->config->get('config_gtalk');
 		
 		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
 			$this->data['code'] = str_replace('http', 'https', html_entity_decode($this->config->get('google_talk_code')));
