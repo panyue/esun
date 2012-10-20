4434ca47f03edaa93b7ab9c51c9d6791cb764c23
panyue <iamy.id@gmail.com>
2012-10-19 20:04:25 -0700

    修改了联系方式：添加里德国分部

--- catalog/language/chinese/information/contact.php
+++ catalog/language/chinese/information/contact.php
@@ -4,13 +4,28 @@ $_['heading_title']  = '联系我们';
 
 // Text 
 $_['text_location']  = '我们的地址';
-$_['text_contact']   = '联系我们';
+$_['text_contact']   = '给我们留言';
 $_['text_address']   = '地址：';
 $_['text_email']     = '邮箱：';
-$_['text_telephone'] = '电话：';
+$_['text_telephone'] = '手机：';
 $_['text_fax']       = '传真：';
 $_['text_message']   = '<p>您的查询已发送！</p>';
 $_['text_submit']    = '提交';
+$_['text_department_cn']    = '中国总部';
+$_['text_department_de']    = '欧洲总部';
+
+
+
+
+// content
+$_['content_address_cn'] = '易翔中国<br />广东省汕头市澄海区梅陇路<br />邮编：515835';
+$_['content_telephone_cn'] = '（86）133-5308-6286';
+$_['content_fax_cn'] = '（86）0754-8822-8288';
+$_['content_address_de'] = '易翔中国<br />广东省汕头市澄海区梅陇路<br />邮编：515835';
+$_['content_telephone_de'] = '（86）133-5308-6286';
+$_['content_fax_de'] = '（86）0754-8822-8288';
+$_['content_email_de'] = 'esun@gmail.com';
+$_['content_email_cn'] = 'esun@gmail.com';
 
 // Entry Fields
 $_['entry_name']     = '您的姓名：';
--- catalog/language/de_DE/information/contact.php
+++ catalog/language/de_DE/information/contact.php
@@ -10,6 +10,19 @@ $_['text_email']         = 'E-Mail:';
 $_['text_telephone']     = 'Telefon:';
 $_['text_fax']           = 'Fax:';
 $_['text_message']       = '<p>Die Anfrage wurde erfolgreich versendet!</p>';
+$_['text_department_cn']    = '中国总部';
+$_['text_department_de']    = '欧洲总部';
+
+// content
+$_['content_address_cn'] = '易翔中国<br />广东省汕头市澄海区梅陇路<br />邮编：515835';
+$_['content_telephone_cn'] = '（86）133-5308-6286';
+$_['content_fax_cn'] = '（86）0754-8822-8288';
+$_['content_address_de'] = '易翔中国<br />广东省汕头市澄海区梅陇路<br />邮编：515835';
+$_['content_telephone_de'] = '（86）133-5308-6286';
+$_['content_fax_de'] = '（86）0754-8822-8288';
+$_['content_email_de'] = 'esun@gmail.com';
+$_['content_email_cn'] = 'esun@gmail.com';
+
 
 // Entry Fields
 $_['entry_name']         = 'Name:';
